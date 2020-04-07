45d1c963a7099bb0bf95179c24e2f5a246c48de7
Maximiliano Neiner <maxineiner@gmail.com>
2020-04-07 19:32:03 -0300

    Agregar archivos de proyecto.

---
+++ Entidades/Numero.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        public double valor;
+
+        public Numero(double valor)
+        {
+            this.valor = valor;
+        }
+
+        #region sobrecarga de operadores
+
+        #region igualdad
+
+        public static bool operator ==(Numero n1, Numero n2) 
+        {
+            return n1.valor == n2.valor;
+        }
+
+        public static bool operator !=(Numero n1, Numero n2)
+        {
+            return ! (n1 == n2);
+        }
+
+        public static bool operator ==(Numero n1, Double n2)
+        {
+            return n1.valor == n2;
+        }
+
+        public static bool operator !=(Numero n1, Double n2)
+        {
+            return !(n1 == n2);
+        }
+
+        #endregion
+
+        #region adición y sustracción
+
+        public static Numero operator +(Numero n1, Numero n2)
+        {
+            double suma = n1.valor + n2.valor;
+
+            Numero rta = new Numero(suma);
+
+            return rta;
+        }
+
+        public static Numero operator -(Numero n1, Numero n2)
+        {
+            double resta = n1.valor - n2.valor;
+
+            Numero rta = new Numero(resta);
+
+            return rta;
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ test.sobrecarga.operadores/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace test.sobrecarga.operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Numero n1 = new Numero(5);
+
+            Numero n2 = new Numero(4);
+
+            if (n1 == n2)
+            {
+                Console.WriteLine("son iguales");
+            }
+
+            if (n1 != n2)
+            {
+                Console.WriteLine("son distintos");
+            }
+
+            if (n1 == 5)
+            {
+                Console.WriteLine("son iguales");
+            }
+
+            if (n1 != 8)
+            {
+                Console.WriteLine("son distintos");
+            }
+
+            Numero res = n1 + n2;
+
+            Console.WriteLine("suma = {0}", res.valor);
+
+            res = n1 - n2;
+
+            Console.WriteLine("resta = {0}", res.valor);
+
+            Console.ReadLine();
+        }
+    }
+}
